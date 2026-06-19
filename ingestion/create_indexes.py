c27c46e805ed4ad76dfba21af2fc8a5a47e4fae5
Matheus Leal <lleal.dev@gmail.com>
2026-06-19 20:31:29 -0300

    [feat] Script para criacao de index no qdrante

---
+++ ingestion/create_indexes.py
@@ -0,0 +1,27 @@
+import os
+
+from qdrant_client import QdrantClient, models
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+qdrant = QdrantClient(
+    api_key=os.getenv("QDRANT_API_KEY"),
+    url=os.getenv("QDRANT_URL"),
+)
+
+fields_to_indexs = [
+    "metadata.ticker",
+    "metadata.form_type",
+    "metadata.source",
+]
+
+for field_name in fields_to_indexs:
+    qdrant.create_payload_index(
+        collection_name="financial",
+        field_name=field_name,
+        field_schema=models.PayloadSchemaType.KEYWORD,
+    )
+
+    print(f"Índice criado para {field_name}")
